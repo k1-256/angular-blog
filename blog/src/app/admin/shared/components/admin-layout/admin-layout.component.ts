d7d062ac4d610cfdddc7d48560b7250a3dcc110d
d.trophimov <FyR8PeZGYFJ5GMM>
2021-10-12 23:22:23 +0500

    Lesson 6. Admin Layout

--- blog/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ blog/src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-layout',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  exit(event: PointerEvent) {
+    console.log(event);
+    event.preventDefault();
+    this.route.navigate(['admin', 'login']);
   }
 
 }
