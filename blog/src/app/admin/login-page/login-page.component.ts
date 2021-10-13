58de0b64974d0f3226c2c136351437b50a1f5bb7
d.trophimov <FyR8PeZGYFJ5GMM>
2021-10-13 20:29:38 +0500

    Lesson 7-8. Login Reactive form

--- blog/src/app/admin/login-page/login-page.component.ts
+++ blog/src/app/admin/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IUser } from '../interfaces/user.interface'
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  onSubmit($event) {
+    console.log('beautiful');
+    console.log($event);
+
+    const user: IUser = {
+      email: this.loginForm.get('email').value,
+      password: this.loginForm.get('password').value
+    }
+
+    console.log(user);
   }
 
 }
